3b55201ea7b1ff6cc73bdce72359cb1e7975f86e
DagProgs <114062397+DagProgs@users.noreply.github.com>
2023-03-25 13:33:20 +0300

    Update app.js

--- pushnotification/app.js
+++ pushnotification/app.js
@@ -1,7 +1,7 @@
 
 		Push.create("Hola mundo",{
 			body: "Este es el cuerpo de la notificacion",
-			icon: "img/logo.png",
+			icon: "https://dagprogs.github.io/test/img/logo.png",
 			timeout: 4000,
 			onClick: function () {
 				window.location="https://nickersoft.github.io/push.js/";
