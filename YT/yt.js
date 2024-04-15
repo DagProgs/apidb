b4dfd1ddc3e813b2b59962ef0bd20dbed7748ad1
DagProgs <114062397+DagProgs@users.noreply.github.com>
2024-04-15 07:55:17 +0300

    u

--- YT/yt.js
+++ YT/yt.js
@@ -1,52 +1,30 @@
-const player = new Plyr('#player', {
-    invertTime: false,
-});
-$(function() {
-    // Включение трека по клику
-    $('.change-youtube').click(function(){
-        $('.change-youtube').removeClass('active');
-        $(this).addClass('active');    
-        let youtubeurl = $(this).attr('data-youtube');
-        player.source = {
-            type: 'video',
-            sources: [
-                {
-                    src: youtubeurl,
-                    provider: 'youtube'
-                }
-            ]
-        };    
-        // если нужно запускать видео сразу по клику, раскоментируйте строчки ниже
-		/*
-            window.setTimeout(function() {
-            player.play();
-            }, 1000);  
-				*/
+document.addEventListener('DOMContentLoaded', function() {
+    const player = new Plyr('#player', {
+        enabled: true,
+        controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'],
+        youtube: {
+            noCookie: true,
+            rel: 0,
+            showinfo: 0,
+            iv_load_policy: 3
+        }
     });
-    // Переключение аидео на следующее по окончанию    
-    player.on('ended', event => {
-        let nextyoutube = $('.change-youtube.active').next(".change-youtube");
-        let urlnextyoutube = nextyoutube.attr('data-youtube');
-        if (!urlnextyoutube) {
-            player.stop();     
-            } else {
-            $('.change-youtube').removeClass('active');
-            nextyoutube.addClass('active');
+
+    // Обработка событий клика по миниатюрам видео
+    const thumbnails = document.getElementsByClassName('change-youtube');
+    for (let i = 0; i < thumbnails.length; i++) {
+        thumbnails[i].addEventListener('click', function() {
+            const videoId = this.getAttribute('data-youtube');
             player.source = {
                 type: 'video',
                 sources: [
                     {
-                        src: urlnextyoutube,
-                        provider: 'youtube'
-                    }
-                ]
-            };        
-            // если нужно запускать следующее видео, раскоментируйте строчки ниже
-            /*
-                window.setTimeout(function() {    
-                player.play(); 
-                }, 1000);
-            */
-        }
-    });    
-});    
+                        src: videoId,
+                        provider: 'youtube',
+                    },
+                ],
+            };
+            player.play();
+        });
+    }
+});
